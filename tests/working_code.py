3f2b2eaf1cbd3e34a7d857ffc444205f2fa77319
Keaganzy <Keaganzy@users.noreply.github.com>
2020-09-13 04:24:28 +0800

    binary table created

--- tests/working_code.py
+++ tests/working_code.py
@@ -6,69 +6,55 @@ from datetime import datetime
 
 import threading
 import time
+import json
 
 
 class IBapi(EWrapper, EClient):
-    def __init__(self):
+    def __init__(self, client_id):
         EClient.__init__(self, self)
+        self.client_id = client_id
 
     def nextValidId(self, orderId: int):
         super().nextValidId(orderId)
         self.nextorderId = orderId
         print('The next valid order id is: ', self.nextorderId)
 
+    def updateAccountValue(self, key: str, val: str, currency: str, accountName: str):
+        super().updateAccountValue(key, val, currency, accountName)
+        # print("UpdateAccountValue. Key:", key, "Value:", val,"Currency:", currency, "AccountName:", accountName)
+
     def updatePortfolio(self, contract: Contract, position: float, marketPrice: float, marketValue: float, averageCost: float, unrealizedPNL: float, realizedPNL: float, accountName: str):
         super().updatePortfolio(contract, position, marketPrice, marketValue,averageCost, unrealizedPNL, realizedPNL, accountName)
         
-        
-        if accountName == 'DU2589906' : #put portfolio in child account and update only when position changes
-            if not have_order(childportlist, contract.symbol): #reuse function to find (paiseh was lazy/dont know how to code another find function)
-                
-                    childportlist.append([contract.symbol,position])
-            else :
-                
-                for x in range(len(childportlist)):
-                    if(childportlist[x][0] in contract.symbol):  #needs exact match and not substring. Some symbols can be "B" and some symbols can be "BABA". Different stocks
-                        if not (childportlist[x][1] == position ):
-                            childportlist[x] = [contract.symbol,position]
-        for x in range(len(childportlist)): 
-            print('child',childportlist[x])
-
-        if accountName == 'DU2309508' : #put portfolio in master account and update only when position changes
-            if not have_order(portlist, contract.symbol):
-                
-                    portlist.append([contract.symbol,position])
-            else :
-                
-                for x in range(len(portlist)):
-                    if(portlist[x][0] in contract.symbol): #needs exact match and not substring. Some symbols can be "B" and some symbols can be "BABA". Different stocks
-                        if not (portlist[x][1] == position ):
-                            portlist[x] = [contract.symbol,position]
-        for x in range(len(portlist)): 
-            print('Master',portlist[x])
-       
-        # for child in child_details:
-            
-        #     i = len(child["positions"])
-        #     for i in child["positions"]:
-        #         child["positions"].append([contract, position, marketPrice, marketValue,averageCost, unrealizedPNL, realizedPNL, accountName])
-        #     print("asdasdasdsad",child["positions"])
-
-        # child_details['positions']['contract'] = contract
-        # print(child_details['positions']['contract'])
-
-        # print("UpdatePortfolio.", "Symbol:", contract.symbol, "SecType:", contract.secType, "Exchange:", contract.exchange, "Position:", position, "MarketPrice:", marketPrice,
-        #            "MarketValue:", marketValue, "AverageCost:", averageCost,
-        #           "UnrealizedPNL:", unrealizedPNL, "RealizedPNL:", realizedPNL,
-        #           "AccountName:", accountName)
+        if self.client_id == 0: #master update positions size
+            try:
+                master_details['positions'][contract.symbol] = position
+            except:
+                master_details['positions'] = {contract.symbol: position}
+            pass
+            # Master ID
+        else:
+            try:
+                child_details[self.client_id - 1]['positions'][contract.symbol] = position #child update positions size based on clientID
+            except:
+                child_details[self.client_id - 1]['positions'] = {contract.symbol: position}
+      
+        print("UpdatePortfolio.", "Symbol:", contract.symbol, "SecType:", contract.secType, "Exchange:", contract.exchange, "Position:", position, "MarketPrice:", marketPrice,
+                   "MarketValue:", marketValue, "AverageCost:", averageCost,
+                  "UnrealizedPNL:", unrealizedPNL, "RealizedPNL:", realizedPNL,
+                  "AccountName:", accountName)
 
     def updateAccountTime(self, timeStamp: str):
         super().updateAccountTime(timeStamp)
-        print("UpdateAccountTime. Time:", timeStamp)
+        # print("UpdateAccountTime. Time:", timeStamp)
 
     def accountDownloadEnd(self, accountName: str):
         super().accountDownloadEnd(accountName)
         print("AccountDownloadEnd. Account:", accountName)
+        # child_details[0]['all_positions']['FB']= 9.0
+        # for child in child_details:
+        #     print(accountName,child["account_name"],";",json.dumps(child['all_positions']))
+            
 
     def orderStatus(self, orderId, status, filled, remaining, avgFullPrice, permId, parentId, lastFillPrice, clientId, whyHeld, mktCapPrice):
             # need to code cancelling orders here based on orderid (but need to first store pairs)
@@ -86,13 +72,16 @@ class IBapi(EWrapper, EClient):
     def openOrder(self, orderId, contract, order, orderState):
             # Keagan Edit
             # When this is triggered can try to create order to other clients
-            
+            # print('openOrder id:', orderId, contract.symbol, contract.secType, '@', contract.exchange, ':', order.action, order.orderType, order.totalQuantity, orderState.status)    
             order_str = f'Open Order to Child:\n' + \
                         f'Master Order ID: {orderId}\n' + \
                         f'Symbol: {contract.symbol} [ {contract.secType} @ {contract.exchange} ]\n' + \
                         f'Action: {order.action} {order.totalQuantity} [{order.orderType}]\n' + \
                         f'Status: {orderState.status}\n\n'
             
+            # # if orderState.status == 'PreSubmitted':
+            # #     pass
+            # else:
             for child in child_details:
                 if isinstance(child['app'].nextorderId, int) and orderId < 0:
                     print(f'Manual Order Number {orderId} Received')
@@ -103,7 +92,10 @@ class IBapi(EWrapper, EClient):
                         print(child['order_list'], '\n\n\n')
                         
                         if contract.secType == "STK":
-                            order.totalQuantity //= child["riskdivide"] #need to account for % mod values to prevent misallignment of position sizing (etc B>23 = 4child, B>22 = 4child. S> 22+23 = 45 = 9child. EXTRA ONE... so position -1 instead of 0)
+                            if(contract.symbol in (child['binary_indicator'].keys())): #search dict for contract
+                                print('Found binary indicator')
+
+                            order.totalQuantity //= child["risk_divide"] #need to account for % mod values to prevent misallignment of position sizing (etc B>23 = 4child, B>22 = 4child. S> 22+23 = 45 = 9child. EXTRA ONE... so position -1 instead of 0)
                             #regardless of -ve or +ve will reallign. if zero we go zero.
 
                             #Wrote draft code in words. Will explain
@@ -112,8 +104,8 @@ class IBapi(EWrapper, EClient):
                         child['app'].placeOrder(child['app'].nextorderId, contract, order) #place order based on client 0 order
                         child['app'].reqIds(child['app'].nextorderId) #reqID increments the next validId *some error.. the api calls this 3 times per trade i do. fking retard. might be because of the threading also. need to do some self check on -id
 
-            print(order_str)
-            # print('openOrder id:', orderId, contract.symbol, contract.secType, '@', contract.exchange, ':', order.action, order.orderType, order.totalQuantity, orderState.status)
+                print(order_str)
+        
             
 
     def execDetails(self, reqId, contract, execution):
@@ -129,7 +121,7 @@ def have_order(child_list, orderId):
     return False
     
 def child_connect(child_details):
-    temp_app = IBapi()
+    temp_app = IBapi(child_details['client_id'])
     temp_app.connect(child_details['ip_address'], child_details['port'], child_details['client_id'])
     temp_app.nextorderId = None
     
@@ -154,14 +146,11 @@ master_details = {
     'ip_address': '127.0.0.1',
     'port': 7498,
     'client_id': 0,
-    'positions' : {
-        # 'contract' : 
-        # contract, position, marketPrice, marketValue,averageCost, unrealizedPNL, realizedPNL, accountName
-        
-    } #dictionary
+    'account_name' : None,
+    'positions' : {}
 }
 
-master_app = IBapi()
+master_app = IBapi(master_details['client_id'])
 master_app.connect(master_details['ip_address'], master_details['port'], master_details['client_id'])
 master_app.nextorderId = None
 
@@ -184,28 +173,16 @@ child_details = [
         'ip_address': '220.255.254.206',
         'port': 7499,
         'client_id': 1,
-        'riskdivide' : 5,
-        'positions' :  #dunno how to use
-        {
-            'contract' : None,
-            'position' : None, 
-            'marketPrice' : None,
-            'marketValue' : None,
-            'averageCost' : None,
-            'unrealizedPNL' : None,
-            'realizedPNL' : None,
-            'accountName' : None,
-        }
+        'account_name' : None,
+        'risk_divide' : 5,
+        "positions": {},
+        'binary_indicator' : {}
     }
 ]
 
 for i, child_det in enumerate(child_details):
     child_details[i]['app'] = child_connect(child_det)
     child_details[i]['order_list'] = []
-    child_details[i]['all_positions'] = []
-
-childportlist = []
-portlist = []
 
 master_app.reqAutoOpenOrders(True) #this allows to know what orders are placed because orderid will show up -ve
 
@@ -215,3 +192,26 @@ for child in child_details:
 master_app.reqAccountUpdates(True, master_app.clientId)# request postions will call updatePortfolio()
 
 time.sleep(3)
+
+for child in child_details: #gets the difference in dict to find positions not the same
+    A = list(child['positions'].keys())
+    B = list(master_details['positions'].keys())
+    commonKeys = set(A) - (set(A) - set(B))
+    for key in commonKeys:
+        if((child['positions'][key]) * child['risk_divide'] != (master_details['positions'][key])):
+            print(key ,":" ,(child['positions'][key]) , " should be " , (master_details['positions'][key]), "risk mul:", child['risk_divide'])
+            
+            if((child['positions'][key]) > 0 and master_details['positions'][key] > 0 ): #give binary to child
+                child['binary_indicator'][key] = [0,0]
+                print(json.dumps(child['binary_indicator']))
+            if((child['positions'][key]) > 0 and master_details['positions'][key] < 0 ):
+                child['binary_indicator'][key] = [0,1]
+            if((child['positions'][key]) < 0 and master_details['positions'][key] > 0 ):
+                child['binary_indicator'][key] = [1,0]
+            if((child['positions'][key]) < 0 and master_details['positions'][key] < 0 ):
+                child['binary_indicator'][key] = [1,1]          
+
+for child in child_details:
+    print(json.dumps(child['positions']))
+
+print(json.dumps(master_details['positions']))
